a5c08012b8e7b2fd23a5da52cee4535f88da9004
Herval <hervalmataamparo@gmail.com>
2020-01-30 14:54:19 -0300

    Route model bind

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace LacosFofos\Providers;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use LacosFofos\Models\Category;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -26,6 +28,11 @@ class RouteServiceProvider extends ServiceProvider
         //
 
         parent::boot();
+
+        Route::bind('category', function ($value){
+            /** @var Collection $collection */
+            $collection = Category::whereId($value)->orWhere('slug', $value)->get();
+           return $collection->first();
     }
 
     /**
