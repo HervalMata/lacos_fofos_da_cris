f8210a896c74142cebbe16da0155cd6f02e59724
Herval <hervalmataamparo@gmail.com>
2020-01-30 14:11:09 -0300

    Trabalhando com slugs

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,23 @@
 <?php
-
+declare(strict_types=1);
 namespace LacosFofos\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'slug', 'active'];
+    use Sluggable;
+    protected $fillable = ['name', 'active'];
+
+    /**
+     * @inheritDoc
+     */
+    public function sluggable(): array
+    {
+        return ['slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
