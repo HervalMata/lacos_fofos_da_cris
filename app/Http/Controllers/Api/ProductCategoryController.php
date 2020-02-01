e3eedc500684f051c9ca5cfd8b24be29eecde8e2
Herval <hervalmataamparo@gmail.com>
2020-02-01 17:00:03 -0300

    Criando endpoints de relaciomamento entre produtos e categorias

---
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LacosFofos\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use LacosFofos\Http\Controllers\Controller;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
