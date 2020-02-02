debd38bb7cbe5cdf9b1e2b2950d6d626e3ee8bd6
Herval <hervalmataamparo@gmail.com>
2020-02-02 16:03:15 -0300

    Inserindo categorias num produto

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -2,8 +2,12 @@
 
 namespace LacosFofos\Http\Controllers\Api;
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use LacosFofos\Http\Controllers\Controller;
+use LacosFofos\Models\Category;
 use LacosFofos\Models\Product;
 
 class ProductCategoryController extends Controller
@@ -11,7 +15,7 @@ class ProductCategoryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index(Product $product)
     {
@@ -21,19 +25,24 @@ class ProductCategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Product $product
+     * @return array|JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, Product $product)
     {
-        //
+        $changed = $product->categories()->sync($request->categories);
+        $categoriesAttachedIds = $changed['attached'];
+        /** @var Collection $categories */
+        $categories = Category::whereIn('id', $categoriesAttachedIds)->get();
+        return $categories->count() ? \response()->json($categories, 201) : [];
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
