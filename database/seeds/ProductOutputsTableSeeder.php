296542060a42ead1369a5d9630a83b3dedf246fb
Herval <hervalmataamparo@gmail.com>
2020-02-02 21:27:37 -0300

    Criando saida de estoque

---
+++ database/seeds/ProductOutputsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use LacosFofos\Models\Product;
+use LacosFofos\Models\ProductOutput;
+
+class ProductOutputsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = Product::all();
+        factory(ProductOutput::class, 159)
+            ->make()
+            ->each(function ($output) use($products) {
+                $product = $products->random();
+                $output->product_id = $product->id;
+                $output->save();
+            });
+    }
+}
