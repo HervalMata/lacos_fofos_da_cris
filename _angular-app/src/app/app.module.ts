b735488c9e9ff934bd7d2767bedfbe2e96db12ed
Herval <hervalmataamparo@gmail.com>
2020-02-06 19:55:29 -0300

    Listando categorias no componentes

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
