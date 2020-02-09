d2f0add415e3bd5786d6189fd423aa33cf0d7430
Herval <hervalmataamparo@gmail.com>
2020-02-09 00:03:16 -0300

    Criando componente para exclusão de categorias

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './components/pages/login/login.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { CategoryListComponent } from './pages/category/category-list/category-list.component';
+import { CategoryListComponent } from './components/pages/category/category-list/category-list.component';
 import {RouterModule, Routes} from "@angular/router";
-import { AlertErrorComponent } from './bootstrap/alert-error/alert-error.component';
-import { ModalComponent } from './bootstrap/modal/modal.component';
-import {CategoryNewModalComponent} from "./pages/category/category-new-modal/category-new-modal.component";
-import {CategoryEditModalComponent} from "./pages/category/category-edit-modal/category-edit-modal.component";
+import { AlertErrorComponent } from './components/bootstrap/alert-error/alert-error.component';
+import { ModalComponent } from './components/bootstrap/modal/modal.component';
+import {CategoryNewModalComponent} from "./components/pages/category/category-new-modal/category-new-modal.component";
+import {CategoryEditModalComponent} from "./components/pages/category/category-edit-modal/category-edit-modal.component";
+import { CategoryDeleteModalComponent } from './components/pages/category/category-delete-modal/category-delete-modal.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
@@ -26,7 +27,8 @@ const routes: Routes = [
     AlertErrorComponent,
     ModalComponent,
     CategoryNewModalComponent,
-    CategoryEditModalComponent
+    CategoryEditModalComponent,
+    CategoryDeleteModalComponent
   ],
   imports: [
     BrowserModule,
--- _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {CategoryNewModalComponent} from "../category-new-modal/category-new-modal.component";
 import {CategoryEditModalComponent} from "../category-edit-modal/category-edit-modal.component";
+import {CategoryDeleteModalComponent} from "../category-delete-modal/category-delete-modal.component";
 
 declare let $;
 
@@ -23,6 +24,9 @@ export class CategoryListComponent implements OnInit {
 
   @ViewChild(CategoryEditModalComponent)
   categoryEditModal: CategoryEditModalComponent;
+
+  @ViewChild(CategoryDeleteModalComponent)
+  categoryDeleteModal: CategoryDeleteModalComponent;
 
   categoryId: number;
 
@@ -51,6 +55,11 @@ export class CategoryListComponent implements OnInit {
     this.categoryEditModal.showModal();
   }
 
+  showModalDelete(categoryId: number) {
+    this.categoryId = categoryId;
+    this.categoryDeleteModal.showModal();
+  }
+
   onInsertSuccess($event: any) {
     console.log($event);
     this.getCategories();
@@ -66,6 +75,15 @@ export class CategoryListComponent implements OnInit {
   }
 
   onEditError($event: HttpErrorResponse) {
+    console.log($event);
+  }
+
+  onDeleteSuccess($event: any) {
+    console.log($event);
+    this.getCategories();
+  }
+
+  onDeleteError($event: HttpErrorResponse) {
     console.log($event);
   }
 }
