79ce84b3b3df3b638be470587842f3851c52f28c
Herval <hervalmataamparo@gmail.com>
2020-02-10 14:49:25 -0300

    Pegando o nome do usuario autenticado

--- _angular-app/src/app/app.component.ts
+++ _angular-app/src/app/app.component.ts
@@ -1,17 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import pace from 'pace';
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'angular-app';
+
+  constructor(public authService: AuthService) {
+  }
 
   ngOnInit(): void {
     pace.start({
-      document:false
+      document: false
     })
   }
 }
--- _angular-app/src/app/services/auth.service.ts
+++ _angular-app/src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {tap} from "rxjs/operators";
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {User} from "../model";
 
 const TOKEN_KEY = 'lacos_fofos';
 
@@ -10,7 +12,11 @@ const TOKEN_KEY = 'lacos_fofos';
 })
 export class AuthService {
 
+  me: User = null;
+
   constructor(private http: HttpClient) {
+    const token = this.getToken();
+    this.setUserFromToken(token);
   }
 
   login(user: { email: string, password: string }): Observable<{ token: string }> {
@@ -23,10 +29,21 @@ export class AuthService {
   }
 
   setToken(token: string) {
+    this.setUserFromToken(token);
     window.localStorage.setItem(TOKEN_KEY, token);
   }
 
   getToken(): string | null {
     return window.localStorage.getItem(TOKEN_KEY);
+  }
+
+
+  private setUserFromToken(token: string) {
+    const decodedToken = new JwtHelperService().decodeToken(token);
+    this.me = decodedToken ? {
+      id: decodedToken.sub,
+      name: decodedToken.name,
+      email: decodedToken.email,
+    } : null;
   }
 }
