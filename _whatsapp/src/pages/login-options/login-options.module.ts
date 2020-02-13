71f1232d321cf4b07fde5484c8c85182600bdaf3
Herval <hervalmataamparo@gmail.com>
2020-02-13 19:18:07 -0300

    Definindo navegacao para opcao de login

---
+++ _whatsapp/src/pages/login-options/login-options.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {LoginOptionsPage} from './login-options';
+
+@NgModule({
+  declarations: [
+    LoginOptionsPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(LoginOptionsPage),
+  ],
+})
+export class LoginOptionsPageModule {
+}
---
+++ _whatsapp/src/pages/reset-phone-number/reset-phone-number.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {ResetPhoneNumberPage} from './reset-phone-number';
+
+@NgModule({
+  declarations: [
+    ResetPhoneNumberPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ResetPhoneNumberPage),
+  ],
+})
+export class ResetPhoneNumberPageModule {
+}
